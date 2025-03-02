c1f0d0054d1f8b8e76ab6ecebb0fad4d9b137d03
om santoshwar <omsantoshwar02@gmail.com>
2025-03-02 23:59:31 +0530

    fix the error with modified reverse function

--- 6-fuzz/reverse_test.go
+++ 6-fuzz/reverse_test.go
@@ -26,9 +26,22 @@ func FuzzReverse(f *testing.F) {
 	for _, tc := range testcases {
 		f.Add(tc) // Use f.Add to provide a seed corpus
 	}
+	// 	f.Fuzz(func(t *testing.T, orig string) {
+	//     rev := Reverse(orig)
+	//     doubleRev := Reverse(rev)
+	//     t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+	//     if orig != doubleRev {
+	//         t.Errorf("Before: %q, after: %q", orig, doubleRev)
+	//     }
+	//     if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+	//         t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+	//     }
+	// })
+
 	f.Fuzz(func(t *testing.T, orig string) {
 		rev := Reverse(orig)
 		doubleRev := Reverse(rev)
+		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
 		if orig != doubleRev {
 			t.Errorf("Before: %q, after: %q", orig, doubleRev)
 		}
